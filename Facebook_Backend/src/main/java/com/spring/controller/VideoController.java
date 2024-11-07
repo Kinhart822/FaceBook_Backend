3e6bbd4822cc92be865ef202ecaf8abf9588cbdd
minoxd <minophen.aceta114@gmail.com>
2024-11-07 14:10:03 +0700

    feat: controller for videos

---
+++ Facebook_Backend/src/main/java/com/spring/controller/VideoController.java
@@ -0,0 +1,48 @@
+package com.spring.controller;
+
+import com.spring.config.JwtUtil;
+import com.spring.dto.Request.VideoRequest;
+import com.spring.dto.Response.CommonResponse;
+import com.spring.entities.Video;
+import com.spring.service.VideoService;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
+public class VideoController {
+    private final VideoService videoService;
+    private final JwtUtil jwtUtil;
+
+    @PostMapping("/video")
+    public ResponseEntity<CommonResponse> createVideo(@RequestBody VideoRequest videoRequest, HttpServletRequest request) {
+        Integer userId = jwtUtil.getUserIdFromToken(request);
+        return ResponseEntity.ok(videoService.createVideo(userId, videoRequest));
+    }
+
+    @GetMapping("/video")
+    public List<Video> getAllVideos() {
+        return videoService.getAllVideos();
+    }
+
+    @GetMapping("/video/{id}")
+    public Video getVideo(@PathVariable Integer id) {
+        return videoService.getVideo(id);
+    }
+
+    @PutMapping("/video/{id}")
+    public ResponseEntity<CommonResponse> updateVideo(@PathVariable Integer id, @RequestBody VideoRequest videoRequest, HttpServletRequest request) {
+        Integer userId = jwtUtil.getUserIdFromToken(request);
+        return ResponseEntity.ok(videoService.updateVideo(id, userId, videoRequest));
+    }
+
+    @DeleteMapping("/video/{id}")
+    public ResponseEntity<CommonResponse> deleteVideo(@PathVariable Integer id) {
+        return ResponseEntity.ok(videoService.deleteVideo(id));
+    }
+}
